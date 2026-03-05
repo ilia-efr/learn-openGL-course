3b709b409b7e29db10a2591fd6b06cd3be52ed58
Ilia <iliafm.efr@gmail.com>
2026-03-05 17:04:20 +0200

    interpolated "Hello triangle"

--- main.cpp
+++ main.cpp
@@ -6,45 +6,23 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-const char *vertexShaderSource ="#version 330 core\n"
-                                "layout (location = 0) in vec3 aPos;\n"
-                                "void main()\n"
-                                "{\n"
-                                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                "}\0";
 
-const char *fragmentShaderSource1 = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "    FragColor = vec4(0.0f, 0.0f, 1.0f, 1"
-                                   ".0f);\n"
-                                   "} ";
-
-const char *fragmentShaderSource2 = "#version 330 core\n"
-                                    "out vec4 FragColor;\n"
-                                    "\n"
-                                    "void main()\n"
-                                    "{\n"
-                                    "    FragColor = vec4(1.0f, 0.0f, 0.0f, "
-                                    "1.0f);\n"
-                                    "} ";
-
-
-const char *vertColorPass = "#version 330 core\n"
+const char *vertShaderSrc = "#version 330 core\n"
                             "layout (location = 0) in vec3 aPos;\n"
+                            "layout (location = 1) in vec3 aColor;\n"
+                            "out vec3 ourColor;\n"
                             "void main()\n"
                             "{\n"
                             "   gl_Position = vec4(aPos, 1.0f);\n"
+                            "   ourColor = aColor;"
                             "}\0";
 
-const char *fragColorPass = "#version 330 core\n"
+const char *fragShaderSrc = "#version 330 core\n"
                            "out vec4 FragColor;\n"
-                           "uniform vec4 ourColor;\n"
+                           "in vec3 ourColor;\n"
                            "void main()\n"
                            "{\n"
-                           "FragColor = ourColor;\n"
+                           "    FragColor = vec4(ourColor, 1.0f);\n"
                            "}\n\0";
 
 int main()
@@ -81,7 +59,7 @@ int main()
     // -- vertex --
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertColorPass, NULL);
+    glShaderSource(vertexShader, 1, &vertShaderSrc, NULL);
     glCompileShader(vertexShader);
 
     // -- vertex compilation success check --
@@ -97,18 +75,18 @@ int main()
     }
 
     // -- fragment --
-    unsigned int fragmentShader1;
-    fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader1, 1, &fragColorPass, NULL);
-    glCompileShader(fragmentShader1);
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(fragmentShader);
 
 
-    glGetShaderiv(fragmentShader1, GL_COMPILE_STATUS, &success);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 
     if(!success)
     {
-        glGetShaderInfoLog(fragmentShader1, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAG1::COMPILATION FAILED \n" <<
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAG::COMPILATION FAILED \n" <<
                   std::endl;
     }
 
@@ -118,7 +96,7 @@ int main()
     shaderProgram1 = glCreateProgram();
 
     glAttachShader(shaderProgram1, vertexShader);
-    glAttachShader(shaderProgram1, fragmentShader1);
+    glAttachShader(shaderProgram1, fragmentShader);
     glLinkProgram(shaderProgram1);
 
     glGetShaderiv(shaderProgram1, GL_COMPILE_STATUS, &success);
@@ -132,7 +110,7 @@ int main()
 
     // -- shader cleanup --
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader1);
+    glDeleteShader(fragmentShader);
 
     // -- bg color definition --
     float r = 0.2f;
@@ -142,9 +120,9 @@ int main()
 
     // -- vertex defintion --
     float vertices[] = {
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
     };
 
 
@@ -153,14 +131,18 @@ int main()
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
-    // -- first triangle setup
+    // triangle setup
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+                          (void*)(3 * sizeof(float )));
+    glEnableVertexAttribArray(1);
 
     // -- debug for wireframe mode --
 //    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
