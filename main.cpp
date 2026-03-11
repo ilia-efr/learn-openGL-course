dd9e96db07e66fb2d301b3095e297532d864edda
Ilia <iliafm.efr@gmail.com>
2026-03-11 21:24:24 +0200

    reset the cubes for camera part

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
+#include <iostream>
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
 
+#include <GLFW/glfw3.h>
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+
 #include "glm/gtc/type_ptr.hpp"
 #include "stb_image.h"
-
-#include <iostream>
-
 #include "Shader.h"
 
+
+// -- global variables
+double lastX = 400;
+double lastY = 300;
+
+// -- function definitions --
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 int processInput(GLFWwindow* window);
 void mouse_callback(GLFWwindow* window, double xPosIn, double yPosIn);
-
-double lastX = 400;
-double lastY = 300;
 
 int main()
 {
@@ -24,7 +26,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr,
                                           nullptr);
@@ -42,13 +43,8 @@ int main()
     }
 
     glViewport(0, 0, 800, 600);
-
-
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-
     glEnable(GL_DEPTH_TEST);
-
     glfwSetCursorPosCallback(window, mouse_callback);
 
     // -- SHADERS --
@@ -56,23 +52,14 @@ int main()
                          "frag_shader.fs");
 
     // -- bg color definition --
-    float r = 0.2f;
-    float g = 0.3f;
-    float b = 0.3f;
-    float a = 1.0f;
+    float bgR = 0.2f;
+    float bgG = 0.3f;
+    float bgB = 0.3f;
+    float bgA = 1.0f;
 
-    // -- vertex defintion --
-//    float vertices[] = {
-//            // positions          // colors           // texture1 coords
-//            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-//            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom
-//            // right
-//            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-//            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
-//    };
-
-
+    // -- cube vertex data --
     float vertices[] = {
+            //    x         y       z           s           t
             -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
             0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
             0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
@@ -115,12 +102,7 @@ int main()
             -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
             -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-
-    unsigned int indices[] = {
-            0, 1, 3,
-            1, 2, 3
-    };
-
+    // -- different cube positions --
     glm::vec3 cubePositions[] = {
             glm::vec3( 0.0f,  0.0f,  0.0f),
             glm::vec3( 2.0f,  5.0f, -15.0f),
@@ -134,44 +116,43 @@ int main()
             glm::vec3(-1.3f,  1.0f, -1.5f)
     };
 
+    unsigned int indices[] = {
+            0, 1, 3,
+            1, 2, 3
+    };
+
     // -- Buffer Setup --
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
 
     // triangle setup
     glBindVertexArray(VAO);
-
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-                 GL_STATIC_DRAW);
-
 
     // -- position attribute --
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                           (void*)0);
     glEnableVertexAttribArray(0);
+
      // -- texture1 coords attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
                           (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-
     // -- texture1 data loading --
     unsigned int texture1, texture2;
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
-
-// -- texture1 parameter settings --
+    // -- texture1 parameter settings --
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
+    // -- texture1 mag and min settings
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
+    // -- loading texture1 image
     int width, height, nrChannel;
     stbi_set_flip_vertically_on_load(true);
     unsigned char *data = stbi_load("srcimages/container.jpg",
@@ -189,14 +170,14 @@ int main()
     }
     stbi_image_free(data);
 
-
+    // -- texture2 creation
     glGenTextures(1, &texture2);
     glBindTexture(GL_TEXTURE_2D, texture2);
 
     // -- texture2 parameter settings --
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
+    // -- texture 2 mag min settings
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
@@ -215,23 +196,19 @@ int main()
     stbi_image_free(data);
 
 
+    // -- camera setup --
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDir = glm::normalize(cameraPos - cameraTarget);
+
     // -- debug for wireframe mode --
 //    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    int res;
-    float mixValue = 0.2f;
-
-    //                          -- RENDER LOOP --
+    // -- RENDER LOOP --
     while(!glfwWindowShouldClose(window)){
-        res = processInput(window);
 
-        if(res == 2 && mixValue < 1.0f) mixValue += 0.001f;
-        if(res == 3 && mixValue > 0.0f) mixValue -= 0.001f;
 
-        float rotX = ((float)lastX / 800.0f) * 360.0f - 180.0f;
-        float rotY = ((float)lastY / 600.0f) * 180.0f - 90.0f;
-
-        glClearColor(r, g, b, a);
+        glClearColor(bgR, bgG, bgB, bgA);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glActiveTexture(GL_TEXTURE0);
@@ -239,10 +216,11 @@ int main()
 
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
-        // --triangle draw--
+
+        int inputRes;
         shaderProgram.use();
 
-        shaderProgram.setFloat("mixValue", mixValue);
+        shaderProgram.setFloat("mixValue", 0.2f); // just send a base value atm
 
         glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture1"), 0);
         glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture2"), 1);
@@ -255,31 +233,29 @@ int main()
         int vLoc = glGetUniformLocation(shaderProgram.ID, "V");
         int pLoc = glGetUniformLocation(shaderProgram.ID, "P");
 
-        auto M = glm::mat4(1.0f);
 
         glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(V));
         glUniformMatrix4fv(pLoc, 1, GL_FALSE, glm::value_ptr(P));
 
         glBindVertexArray(VAO);
 
-        M = glm::mat4(1.0f);
-        M = glm::translate(M, cubePositions[0]);
+        for(unsigned int i = 0; i < std::size(cubePositions); i++)
+        {
+            auto M = glm::mat4(1.0f);
+            M = glm::translate(M, cubePositions[i]);
+            float angle = 20.0f * i;
+            M = glm::rotate(M, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shaderProgram.setMat4("M", M);
+            glDrawArrays(GL_TRIANGLES, 0, 36); // no element draw
+        }
 
-        M = glm::rotate(glm::mat4(1.0f), glm::radians(rotX), glm::vec3(0.0f, 1.0f, 0.0f)); // Yaw
-        M = glm::rotate(M, glm::radians(rotY), glm::vec3(1.0f, 0.0f, 0.0f));
-
-        shaderProgram.setMat4("M", M);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
+        inputRes = processInput(window);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
     shaderProgram.Delete();
 
     glfwTerminate();
